2c5d9fd647faaf03d462471459a521714e4ae56e
vrees <viktor.rees@gmx.de>
2020-12-20 18:03:44 +0100

    extracted display.c / .h

---
+++ src/display.h
@@ -0,0 +1,15 @@
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void task_test_SSD1306i2c(void *ignore);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DISPLAY_H_ */
